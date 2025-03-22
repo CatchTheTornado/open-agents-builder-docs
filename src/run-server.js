f80c1b885eca760ac23b0157a407b8256486a5d1
pkarw <piotr@catchthetornado.com>
2025-03-22 09:33:00 +0100

    [feat] github hook

--- src/run-server.js
+++ src/run-server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { handler as ssrHandler } from '../dist/server/entry.mjs';
+const { exec } = require('child_process');
 
 const app = express();
 // Change this based on your astro.config.mjs, `base` option.
@@ -7,5 +8,28 @@ const app = express();
 const base = '/';
 app.use(base, express.static('dist/client/'));
 app.use(ssrHandler);
+
+app.post('/github-webhook', (req, res) => {
+    const script = `
+        cd /home/openagentsbuilder-docs/open-agents-builder-docs
+        pwd
+        git pull
+        npm install
+        npm run build
+        pm2 delete OpenAgentsBuilderDocs
+        pm2 start "node ./src/run-server.js" --name OpenAgentsBuilderDocs
+        pm2 reload all
+    `;
+
+    exec(script, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing script: ${error}`);
+            return res.status(500).send('Deployment failed');
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+        res.send('Deployment successful');
+    });
+});
 
 app.listen(4321);
